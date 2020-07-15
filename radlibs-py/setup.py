92f38ed33597e723bbad44679640a271f7181df2
Stephanie Kirmer <skirmer@journera.com>
2020-07-15 12:47:44 -0500

    setting consistency with py

--- radlibs-py/setup.py
+++ radlibs-py/setup.py
@@ -3,19 +3,19 @@
 
 from setuptools import setup
 
-setup(name = 'radlibs',
-    version = '0.2.0',
-    description = 'Build Your Own Madlibs!',
-    author = 'Stephanie Kirmer',
-    author_email = 'stephanie@stephaniekirmer.com',
-    packages = ['radlibs'],
-    url="https://github.com/skirmer/radlibs-py",
-    install_requires=['pandas',
-                      'feather-format'],
-    package_data={'radlibs': ['data/*.feather']},
-    include_package_data=True,
-    classifiers=[
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: BSD License",
-    "Operating System :: OS Independent",
-    ])
+setup(name='radlibs',
+      version='0.2.0',
+      description='Build Your Own Madlibs!',
+      author='Stephanie Kirmer',
+      author_email='stephanie@stephaniekirmer.com',
+      packages=['radlibs'],
+      url="https://github.com/skirmer/radlibs/tree/master/radlibs-py",
+      install_requires=['pandas',
+                        'feather-format'],
+      package_data={'radlibs': ['data/*.feather']},
+      include_package_data=True,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ])
